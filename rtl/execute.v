b45e6cf385ede89f5c359e15f64ae41bd91b8a22
Ryan Cooke <ryan.cooke@warwick.ac.uk>
2018-08-29 11:59:23 +0100

    added more execture instructions

--- rtl/execute.v
+++ rtl/execute.v
@@ -20,21 +20,37 @@ module execute(
     input clk,
     input rst,
 
-    input [6:0] decode,
+    input [5:0] decode,
     input [7:0] val,
     input [7:0] vx,
     input [7:0] vy,
     input [3:0] x,
-    input [3:0] y,
+
+    output reg [3:0] x_out,
+    output reg wx, //vx write enable
+    output reg [7:0] nx, // new vx data
+    output reg wf,
+    output reg [7:0] wx,
 
     input [11:0] addr,
+    input [15:0] pc_rd,
+    input [7:0] sp_rd,
+    input [15:0] i_rd,
 
-    output [3:0] x_out,
-    output [3:0] y_out,
+    output w_en,
+    output [7:0] w_addr,
+    output [15:0] w_data,
+    output r_en,
+    output [7:0] r_addr,
+    input [15:0] r_data,
 
-    output reg [15:0] pc_out,
-    output reg pc_inc,
-    output reg sp_out
+
+    output reg [15:0] pc_wr,
+    output reg pc_en,
+    output reg [7:0] sp_wr,
+    output reg sp_en,
+    output reg [15:0] i_wr,
+    output reg i_en
   );
 
   localparam DISP_CLR = 6'd1;
@@ -73,9 +89,182 @@ module execute(
   localparam  STORE_REG_VX = 6'd34;
   localparam  READ_REG_VX = 6'd35;
 
+  localparam STACK_OFFSET = 16'd240;
 
+  wire [8:0] add_vx_vy = vx + vy;
+  wire sub_vx_vy_flag = vx < vy;
+  wire sub_vy_vx_flag = vx > vy;
+  //wire [8:0] add_val = vx + val;
+
+  assign r_addr = (decode == RET) ? (sp_rd + STACK_OFFSET) : 0;
+  assign r_en = (decode == RET) ? (sp_rd + STACK_OFFSET) : 0;
+
+ ///////////////////
+ //////////////////
+ //ADD PC INCREMENT AND FETCH PULSE
   always@(posedge clk) begin
-  sp_inc <= 0;
-  pc_inc <= 0;
+    sp_en <= 0;
+    pc_en <= 0;
+    i_en <= 0;
+    wr_en <= 0;
+    wx <= 0;
+    nx <= 0;
+    wf<= 0;
+    wx <= 0;
     case(decode)
       JMP:
+        pc_wr <= addr;
+
+      RET: begin
+        //get stack
+       pc_en <= 1;
+
+
+       //retrieve pc value from top of stack
+
+       //set pc to this value
+       //decrement stack pointer
+
+      CALL: begin
+        sp_en <= 1;
+        sp_wr <= sp_rd + 1;
+        //change names for bram signal
+        w_addr <= STACK_OFFSET + sp_rd;
+        w_data <= pc_rd;
+        w_en <= 1;
+        pc_wr <= addr;
+        pc_en <= 1;
+      end
+
+      SE_VAL:
+        if(vx == val) begin
+          pc_wr <= pc_rd+1;
+          pc_en <= 1;
+        end
+
+      SNE_VAL:
+        if(vx != val) begin
+          pc_wr <= pc_rd+1;
+          pc_en <= 1;
+        end
+
+      SE_VX_VY:
+        if(vx == vy) begin
+          pc_wr <= pc_rd+1;
+          pc_en <= 1;
+        end
+
+      LD_VX_VAL: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= val;
+      end
+
+      ADD_VX_VAL: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vx + val;
+      end
+
+      LD_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vy;
+      end
+
+      OR_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vx | vy;
+      end
+
+      AND_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vx & vy;
+      end
+
+      XOR_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vx ^ vy;
+      end
+
+      ADD_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= add_vx_vy[7:0];
+        //need to store carry in VF
+        wf <= 1;
+        nf <= add_vx_vy[8];
+      end
+
+      SUB_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vx - vy;
+        wf <= 1;
+        nf <= sub_vx_vy_flag;
+      end
+
+      SHR_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vy >> 1;
+        wf <= 1;
+        nf <= vy[0];
+      end
+
+      SUBN_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vy - vx;
+        wf <= 1;
+        nf <= sub_vy_vx_flag;
+      end
+
+      SHL_VX_VY: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= vy << 1;
+        wf <= 1;
+        nf <= vy[7];
+      end
+
+      SNE_VX_VY: begin
+        if(vx != vy) begin
+          pc_wr <= pc_rd+1;
+          pc_en <= 1;
+        end
+      end
+
+      LD_I_ADDR: begin
+        i_en <= 1;
+        i_wr <= {4'd0, addr};
+      end
+
+      JMP_V0_ADDR: begin
+        pc_en <= 1;
+        pc_wr <= addr + vx; //vx in this case is v0
+      end
+
+      RAND_VX_VAL: begin
+        x_out <= x;
+        wx <= 1;
+        nx <= val & rng;
+      end
+
+
+    endcase
+  end
+
+  reg [7:0] rng = 8'b01001010; //seed, make configrable
+  always @(posedge clk)
+   if (rst)
+      rng <= {7{1'b0}};
+   else begin
+      rng <= {count[5:0],~(^(count &  8'b10110001))};
+    end
+  end
+
+  endmodule

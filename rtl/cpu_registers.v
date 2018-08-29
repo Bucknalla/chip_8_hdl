daf339fc8c19f839815e7935d4b78e4fe9638907
Ryan Cooke <ryan.cooke@warwick.ac.uk>
2018-08-29 11:35:27 +0100

    updated pc and sp names, added I reg

--- rtl/cpu_registers.v
+++ rtl/cpu_registers.v
@@ -28,13 +28,17 @@ module cpu_registers(
            output	    [7:0]	Vy,
            output	    [7:0]	Vf,
 
-           input pc_inc,
-           input sp_inc,
-           input sp_dec,
-           input [15:0] pc_in,
-           output   [15:0] pc_out,
-           output   [15:0] sp_out,
+           input pc_en,
+           input sp_en,
 
+           input [15:0] pc_wr,
+           input [7:0] sp_wr,
+           output   [15:0] pc_rd,
+           output   [7:0] sp_rd,
+
+           input [15:0] i_wr,
+           input i_en,
+           output [15:0] i_rd,
 
            input				wx,     // Vx write enable
            input		[7:0]	nx,     // new Vx data
@@ -44,6 +48,7 @@ module cpu_registers(
        );
 
 reg [7:0] Vreg [0:15];
+reg [15:0] i;
 
 reg [15:0] pc = 16'h0; //program counter, holds address of current instruction
 reg [7:0] sp = 8'd0; //stack pointer, holds address of top of stack, stack starts at 0xEA0
@@ -58,15 +63,16 @@ always @ (posedge clk) begin
         Vreg[x] <= nx;
     if (wf)
         Vreg[15] <= nf;
-    if (pc_inc)
-        pc <= pc_in; 
-    if (sp_inc)
-        sp <= sp+1;
-    if (sp_dec)
-        sp <= sp-1;
+    if (pc_en)
+        pc <= pc_wr;
+    if (sp_en)
+        sp <= sp_wr;
+    if (i_en)
+      i <= i_wr;
 end
 
-assign pc_out = pc;
-assign sp_out = STACK_OFFSET + sp;
+assign pc_rd = pc;
+assign sp_rd = sp;
+assign i_rd = i
 
 endmodule

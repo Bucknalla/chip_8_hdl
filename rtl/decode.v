eae7df17a96bcd0690368f8fea31149d974c0ae6
Ryan Cooke <ryan.cooke@warwick.ac.uk>
2018-08-29 09:57:45 +0100

    FInish decode (dont know if works)

--- rtl/decode.v
+++ rtl/decode.v
@@ -29,6 +29,7 @@ module decode(
   output reg [15:0] addr_out,
   output reg [3:0] x,
   output reg [3:0] y,
+  output reg [3:0] nib,
   output reg [7:0] val
 
        );
@@ -52,10 +53,31 @@ localparam  SHR_VX_VY = 5'd16;
 localparam  SUBN_VX_VY = 5'd17;
 localparam  SHL_VX_VY = 5'd18;
 localparam  SNE_VX_VY = 5'd19;
+localparam  LD_I_ADDR = 5'd20;
+localparam  JMP_V0_ADDR = 5'd21;
+localparam  RAND_VX_VAL = 5'd22;
+localparam  DRW_VX_VY = 5'd23;
+localparam  SKP_VX = 5'd24;
+localparam  SKNP_VX = 5'd25;
+localparam  LD_VX_DT = 5'd26;
+localparam  LD_VX_K = 5'd27;
+localparam  LD_DT_VX = 5'd28;
+localparam  LD_ST_VX = 5'd29;
+localparam  ADD_I_VX = 5'd30;
+localparam  LD_ST_VX = 5'd31;
+localparam  LD_F_VX = 5'd32;
+localparam  LD_B_VX = 5'd33;
+localparam  STORE_REG_VX = 5'd34;
+localparam  READ_REG_VX = 5'd35;
 
 //instructions stored with most significat byt
 always@(posedge clk) begin
   decode <= 5'd0;
+  x <= 4'd0;
+  y <= 4'd0;
+  val = 8'd0;
+  nib <= 4'd0;
+  addr_out = 12'd0;
   case(instruction[15:12]) begin
     4'd0: begin
       //zero instructions -
@@ -170,7 +192,7 @@ always@(posedge clk) begin
     end
 
     4'd11: begin
-      decode <= JUMP_V0_ADDR;
+      decode <= JMP_V0_ADDR;
       addr_out <= instruction[11:0];
     end
 
@@ -179,12 +201,51 @@ always@(posedge clk) begin
       addr_out <= instruction[11:0];
     end
 
+    4'd13: begin
+      decode <= DRW_VX_VY;
+      x <= instruction[11:8];
+      y <= instruction[7:4];
+      nib <= instruction[3:0];
+    end
 
+    4'd14: begin
+      x <= instruction[11:8];
+      if(instruction[7:0] == 8'h9E) begin
+        decode <= SKP_VX;
+      end else if(instruction[7:0] == 8'hA1) begin
+        decode <= SKNP_VX;
+      end else begin
+        decode <= 0;
+      end
+    end
 
+    4'd15: begin
+      x <= instruction[11:8];
+      case(instruction[7:0]) begin
+        4'h07:
+          decode <= LD_VX_DT;
+        4'h0A:
+          decode <= LD_VX_K;
+        4'h15:
+          decode <= LD_DT_VX;
+        4'h18:
+          decode <= LD_ST_VX;
+        4'h1E:
+          decode <= ADD_I_VX;
+        4'h29:
+          decode <= LD_F_VX;
+        4'h33:
+          decode <= LD_B_VX;
+        4'h55:
+          decode <= STORE_REG_VX;
+        4'h65:
+          decode <= READ_REG_VX;
+        default:
+          decode <= 0;
+      endcase
+    end
   endcase
 end
-
-
 
 
 endmodule

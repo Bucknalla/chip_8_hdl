7beff6846a97c09fd8afbaf4cf3e2a33d0349635
Ryan Cooke <ryan.cooke@warwick.ac.uk>
2018-08-29 10:08:26 +0100

    fixed decode errors

--- rtl/decode.v
+++ rtl/decode.v
@@ -23,7 +23,7 @@ module decode(
 
   input [15:0] instruction,
 
-  output reg [4:0] decode,
+  output reg [5:0] decode,
 
 
   output reg [15:0] addr_out,
@@ -34,60 +34,58 @@ module decode(
 
        );
 
-localparam DISP_CLR = 5'd1;
-localparam RET = 5'd2;
-localparam JMP = 5'd3;
-localparam CALL = 5'd4;
-localparam SE_VAL = 5'd5;
-localparam SNE_VAL = 5'd6;
-localparam SE_VX_VY = 5'd7;
-localparam LD_VX_VAL = 5'd8;
-localparam ADD_VX_VAL = 5'd9;
-localparam LD_VX_VY = 5'd10;
-localparam  OR_VX_VY = 5'd11;
-localparam  AND_VX_VY = 5'd12;
-localparam  XOR_VX_VY = 5'd13;
-localparam  ADD_VX_VY = 5'd14;
-localparam  SUB_VX_VY = 5'd15;
-localparam  SHR_VX_VY = 5'd16;
-localparam  SUBN_VX_VY = 5'd17;
-localparam  SHL_VX_VY = 5'd18;
-localparam  SNE_VX_VY = 5'd19;
-localparam  LD_I_ADDR = 5'd20;
-localparam  JMP_V0_ADDR = 5'd21;
-localparam  RAND_VX_VAL = 5'd22;
-localparam  DRW_VX_VY = 5'd23;
-localparam  SKP_VX = 5'd24;
-localparam  SKNP_VX = 5'd25;
-localparam  LD_VX_DT = 5'd26;
-localparam  LD_VX_K = 5'd27;
-localparam  LD_DT_VX = 5'd28;
-localparam  LD_ST_VX = 5'd29;
-localparam  ADD_I_VX = 5'd30;
-localparam  LD_ST_VX = 5'd31;
-localparam  LD_F_VX = 5'd32;
-localparam  LD_B_VX = 5'd33;
-localparam  STORE_REG_VX = 5'd34;
-localparam  READ_REG_VX = 5'd35;
+localparam DISP_CLR = 6'd1;
+localparam RET = 6'd2;
+localparam JMP = 6'd3;
+localparam CALL = 6'd4;
+localparam SE_VAL = 6'd5;
+localparam SNE_VAL = 6'd6;
+localparam SE_VX_VY = 6'd7;
+localparam LD_VX_VAL = 6'd8;
+localparam ADD_VX_VAL = 6'd9;
+localparam LD_VX_VY = 6'd10;
+localparam  OR_VX_VY = 6'd11;
+localparam  AND_VX_VY = 6'd12;
+localparam  XOR_VX_VY = 6'd13;
+localparam  ADD_VX_VY = 6'd14;
+localparam  SUB_VX_VY = 6'd15;
+localparam  SHR_VX_VY = 6'd16;
+localparam  SUBN_VX_VY = 6'd17;
+localparam  SHL_VX_VY = 6'd18;
+localparam  SNE_VX_VY = 6'd19;
+localparam  LD_I_ADDR = 6'd20;
+localparam  JMP_V0_ADDR = 6'd21;
+localparam  RAND_VX_VAL = 6'd22;
+localparam  DRW_VX_VY = 6'd23;
+localparam  SKP_VX = 6'd24;
+localparam  SKNP_VX = 6'd25;
+localparam  LD_VX_DT = 6'd26;
+localparam  LD_VX_K = 6'd27;
+localparam  LD_DT_VX = 6'd28;
+localparam  LD_ST_VX = 6'd29;
+localparam  ADD_I_VX = 6'd30;
+localparam  LD_ST_VX = 6'd31;
+localparam  LD_F_VX = 6'd32;
+localparam  LD_B_VX = 6'd33;
+localparam  STORE_REG_VX = 6'd34;
+localparam  READ_REG_VX = 6'd35;
 
 //instructions stored with most significat byt
 always@(posedge clk) begin
-  decode <= 5'd0;
+  decode <= 6'd0;
   x <= 4'd0;
   y <= 4'd0;
   val = 8'd0;
   nib <= 4'd0;
   addr_out = 12'd0;
-  case(instruction[15:12]) begin
+  case(instruction[15:12])
     4'd0: begin
-      //zero instructions -
-      //display clear
-      //return from subroutine
-      if(instruction[3:0] == 4'h0)begin
+      if(instruction[3:0] == 4'h0)
         decode <= DISP_CLR;
-      end else if(instruction[3:0] == 4'hE)
+      else if(instruction[3:0] == 4'hE)
         decode <= RET;
-      end
+      else
+        decode <= 0;
     end
 
     4'd1: begin
@@ -131,7 +129,7 @@ always@(posedge clk) begin
     end
 
     4'd8: begin
-      case(instruction[3:0]) begin
+      case(instruction[3:0])
         4'd0: begin
           decode <= LD_VX_VY;
           val <= instruction[7:0];
@@ -172,7 +170,7 @@ always@(posedge clk) begin
           y <= instruction[7:4];
           x <= instruction[11:8];
         end
-        4'dE: begin
+        4'hE: begin
           decode <= SHL_VX_VY;
           y <= instruction[7:4];
           x <= instruction[11:8];
@@ -221,24 +219,24 @@ always@(posedge clk) begin
 
     4'd15: begin
       x <= instruction[11:8];
-      case(instruction[7:0]) begin
-        4'h07:
+      case(instruction[7:0])
+        8'h07:
           decode <= LD_VX_DT;
-        4'h0A:
+        8'h0A:
           decode <= LD_VX_K;
-        4'h15:
+        8'h15:
           decode <= LD_DT_VX;
-        4'h18:
+        8'h18:
           decode <= LD_ST_VX;
-        4'h1E:
+        8'h1E:
           decode <= ADD_I_VX;
-        4'h29:
+        8'h29:
           decode <= LD_F_VX;
-        4'h33:
+        8'h33:
           decode <= LD_B_VX;
-        4'h55:
+        8'h55:
           decode <= STORE_REG_VX;
-        4'h65:
+        8'h65:
           decode <= READ_REG_VX;
         default:
           decode <= 0;
